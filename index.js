54897bf71ece4f1c335d65b55747df2110731caa
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-02 22:42:33 -0700

    commit tree 6a71d5a2aec547cbb82bf8c7501687a0b1b8d61b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updatedDom();
+  });
+
+
+function updatedDom() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
+
